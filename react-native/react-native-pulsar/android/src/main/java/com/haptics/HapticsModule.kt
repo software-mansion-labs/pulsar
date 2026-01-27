1fde1cce9c6c34f237e28735dccf29e7a969c86b
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2026-01-27 16:38:33 +0100

    Android setup

--- react-native/react-native-pulsar/android/src/main/java/com/haptics/HapticsModule.kt
+++ react-native/react-native-pulsar/android/src/main/java/com/haptics/HapticsModule.kt
@@ -1,20 +1,60 @@
 package com.pulsar
 
 import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReadableArray
+import com.facebook.react.bridge.ReadableMap
 import com.facebook.react.module.annotations.ReactModule
+import com.rnpulsar.NativeRNPulsarSpec
 
 @ReactModule(name = PulsarModule.NAME)
 class PulsarModule(reactContext: ReactApplicationContext) :
-  NativePulsarSpec(reactContext) {
-
-  override fun getName(): String {
-    return NAME
+  NativeRNPulsarSpec(reactContext) {
+  override fun Pulsar_play(name: String?) {
+    TODO("Not yet implemented")
   }
 
-  // Example method
-  // See https://reactnative.dev/docs/native-modules-android
-  override fun multiply(a: Double, b: Double): Double {
-    return a * b
+  override fun Pulsar_enableSound(state: Boolean) {
+    TODO("Not yet implemented")
+  }
+
+  override fun Pulsar_enableCache(state: Boolean) {
+    TODO("Not yet implemented")
+  }
+
+  override fun Pulsar_clearCache(state: Boolean) {
+    TODO("Not yet implemented")
+  }
+
+  override fun Pulsar_preloadPresets(presetNames: ReadableArray?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun RealtimeComposer_update(amplitude: Double, frequency: Double) {
+    TODO("Not yet implemented")
+  }
+
+  override fun RealtimeComposer_stop() {
+    TODO("Not yet implemented")
+  }
+
+  override fun RealtimeComposer_isActive(): Boolean {
+    TODO("Not yet implemented")
+  }
+
+  override fun RealtimeComposer_playDiscrete(amplitude: Double, frequency: Double) {
+    TODO("Not yet implemented")
+  }
+
+  override fun PatternComposer_parsePattern(data: ReadableMap?): Double {
+    TODO("Not yet implemented")
+  }
+
+  override fun PatternComposer_play(patternId: Double) {
+    TODO("Not yet implemented")
+  }
+
+  override fun PatternComposer_release(patternId: Double) {
+    TODO("Not yet implemented")
   }
 
   companion object {
