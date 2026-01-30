77d93061700840c648ae51538a52019fc66a3085
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2026-01-30 13:35:56 +0100

    Decs config start

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -16,22 +16,28 @@ export default defineConfig({
     },
   },
   integrations: [starlight({
-    title: 'Happytic',
+    title: 'Pulsar',
     customCss: [
       './src/pages/index.css',
       './src/content/docs/custom.css',
       '@fontsource/bebas-neue/400.css',
     ],
     pagination: false,
-    social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/swmansion' }],
+    social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/swmansion/pulsar' }],
     sidebar: [
-      
+      {
+        label: 'Getting started',
+        slug: 'getting-started'
+      },
     ],
     logo: {
-      light: './src/assets/logo_v1.png',
-      dark: './src/assets/logo_v1.png',
-      alt: 'TypeGPU Logo',
+      light: './src/assets/logo_label.svg',
+      dark: './src/assets/logo_label.svg',
+      alt: 'Pulsar Logo',
       replacesTitle: true,
+    },
+    components: {
+      ThemeSelect: './src/components/ThemeSelect.astro',
     },
   }),
   react()
