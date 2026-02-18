7ae8b58e87d0160d009b54ff9e9f962afa291838
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2026-02-18 23:00:20 +0100

    Setup pulsar app

--- PulsarApp/app/(tabs)/_layout.tsx
+++ PulsarApp/app/(tabs)/_layout.tsx
@@ -30,13 +30,6 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
-      <Tabs.Screen
-        name="playground"
-        options={{
-          title: 'playground',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
     </Tabs>
   );
 }
