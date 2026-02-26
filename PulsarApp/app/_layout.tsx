631e826f58761f2cd90b168955d83593974e46f8
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2026-02-26 15:58:58 +0100

    Splash screen

--- PulsarApp/app/_layout.tsx
+++ PulsarApp/app/_layout.tsx
@@ -1,6 +1,8 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
@@ -9,12 +11,25 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { FilterProvider } from '@/contexts/FilterContext';
 import { OnboardingProvider } from '@/contexts/OnboardingContext';
 
+// Configure splash screen animation
+SplashScreen.setOptions({
+  duration: 500,
+  fade: true,
+});
+
 export const unstable_settings = {
   anchor: '(tabs)',
 };
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+
+  // SplashScreen.preventAutoHideAsync();
+  
+  useEffect(() => {
+    // Hide splash screen after app is ready
+    SplashScreen.hideAsync();
+  }, []);
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
