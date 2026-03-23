1745932292dabbf9f16947b89fd8316c88ace995
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2026-03-23 10:28:00 +0100

    System preset config template

---
+++ docs/src/content/docs/assets/systemPresets/SystemPresetsConfig.ts
@@ -0,0 +1,14 @@
+import type { PresetConfig } from '../../components/Preset/types';
+
+// CODEGEN_BEGIN_{imports}
+// CODEGEN_END_{imports}
+
+export const AndroidPresetsConfig: Array<PresetConfig> = [
+// CODEGEN_BEGIN_{android_presets}
+// CODEGEN_END_{android_presets}
+];
+
+export const IOSPresetsConfig: Array<PresetConfig> = [
+// CODEGEN_BEGIN_{ios_presets}
+// CODEGEN_END_{ios_presets}
+];
